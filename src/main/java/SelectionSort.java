ba3c0249b34ce68150768aeee34abff87ed88f0d
nihardabhi <dabhi.nihar.5656@gmail.com>
2025-03-20 13:07:22 -0400

    quiz 5 completed

--- src/main/java/SelectionSort.java
+++ src/main/java/SelectionSort.java
@@ -1,8 +1,29 @@
 class SelectionSort {
     public static void selectionSort(int[] arr) {
+        //copy your length of the array so we can iterate in for loop
         int n = arr.length;
 
-       // STUDENT TODO
+        //start to iterate over here
+        for (int i = 0; i < n - 1; i++) {
+            //first let us just assuming that first index element is the smallest of all
+            int minIndex = i;
+
+            //from the very next index let's find the potential smallest element if any
+            for (int j = i + 1; j < n; j++) {
+                //if we can find smallest element exchange that value
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // swap occurs over here
+            //store smallest element in temp for a while
+            int temp = arr[minIndex];
+            //place assumed first sorted element which is not the smallest now, into where we find the smallest elements
+            arr[minIndex] = arr[i];
+            //let's place smallest number into ascending order one by one
+            arr[i] = temp;
+        }
     }
 
     public static void main(String[] args) {
